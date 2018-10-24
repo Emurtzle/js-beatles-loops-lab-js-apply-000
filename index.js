683c80c0ae7090346b002defaca9c5f4e4c8a622
Emily Wagner <emilywagner598@gmail.com>
2018-10-24 18:10:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(a1,a2) {
   var strings = new Array();
-  
+
   var i;
   for (i = 0; i < a1.length; i++) {
     console.log(a1[i] + " plays " + a2[i]);
