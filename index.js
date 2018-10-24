24f7f3a789c1adf30e07d3217fc26b7b6568a75b
Emily Wagner <emilywagner598@gmail.com>
2018-10-24 18:11:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ function theBeatlesPlay(a1,a2) {
   var i;
   for (i = 0; i < a1.length; i++) {
     console.log(a1[i] + " plays " + a2[i]);
+    strings[i] = a1[i] + " plays " + a2[i];
   }
+  return strings;
 }
